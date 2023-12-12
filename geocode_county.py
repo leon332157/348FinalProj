487918ace255c3a4c2a81fb2b069b3baa1d7a725
leon332157 <leon332157@gmail.com>
2023-12-12 14:11:28 -0500

    add plots

--- geocode_county.py
+++ geocode_county.py
@@ -1,16 +1,21 @@
 import pandas as pd
-
 from geopy.geocoders import Nominatim
 
-geolocator = Nominatim(user_agent="info348",timeout=5)  # you may need a timeout argument here!
+"""I needed to seprate out the geocoding script because the geocoding API is slow, 
+and thus instead of running geocoding on the entire dataset every time the notebook is ran,
+I decided to pre-process the data.
+"""
+geolocator = Nominatim(
+    user_agent="info348", timeout=5
+)
 
-county_data = pd.read_csv('county_proprietary_valid_2000_2018.csv')
-loc= geolocator.geocode(f"{county_data['county'][0]}, {county_data['state'][0]}")
+county_data = pd.read_csv("county_proprietary_valid_2000_2018.csv")
+loc = geolocator.geocode(f"{county_data['county'][0]}, {county_data['state'][0]}")
 
 county_dict = {}
 for i in range(len(county_data)):
-    cty = county_data['county'][i]
-    st = county_data['state'][i]
+    cty = county_data["county"][i]
+    st = county_data["state"][i]
     adr = f"{cty}, {st}"
     if adr in county_dict:
         continue
@@ -18,8 +23,12 @@ for i in range(len(county_data)):
     if loc:
         county_dict[adr] = [loc.latitude, loc.longitude]
     else:
-        county_dict[adr] = [0,0]
+        county_dict[adr] = [0, 0]
 
-county_data['lat'] = county_data.apply(lambda row: county_dict[f"{row['county']}, {row['state']}"][0], axis=1)
-county_data['lon'] = county_data.apply(lambda row: county_dict[f"{row['county']}, {row['state']}"][1], axis=1)
-county_data.to_csv('county_data_geolocate.csv', index=False)
+county_data["lat"] = county_data.apply(
+    lambda row: county_dict[f"{row['county']}, {row['state']}"][0], axis=1
+)
+county_data["lon"] = county_data.apply(
+    lambda row: county_dict[f"{row['county']}, {row['state']}"][1], axis=1
+)
+county_data.to_csv("county_data_geolocate.csv", index=False)
